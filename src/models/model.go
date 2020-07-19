256218393d52ede72b9688e063441ff8d0fb98f7
daisuzuki829 <dai.suzuki.829@gmail.com>
2020-07-19 17:22:06 +0900

    heroku

--- src/models/model.go
+++ src/models/model.go
@@ -2,25 +2,34 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
-)
-
-const (
-	Driver = "sqlite3"
-	DbName  = "my_goal.db"
+	"os"
 )
 
 func Open() *gorm.DB {
 	DBMS := "mysql"
-	USER := "root"
-	PASS := "pass"
-	PROTOCOL := "tcp(mysql:3306)"
-	DBNAME := "my_goal"
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true"
-	db, err := gorm.Open(DBMS, CONNECT)
+	var USER string
+	var PASS string
+	var HOST string
+	var DBNAME string
+
+	if os.Getenv("DATABASE_URL") != "" {
+		// Heroku用
+		USER = "b6ccb4c2b8f823"
+		PASS = "378638a9"
+		HOST = "us-cdbr-east-02.cleardb.com"
+		DBNAME = "heroku_114022f1f0797d9"
+	} else {
+		// ローカル用
+		USER = "root"
+		PASS = "pass"
+		HOST = "mysql"
+		DBNAME = "my_goal"
+	}
+
+	dataSource := USER + ":" + PASS + "@tcp(" + HOST + ":3306)/" + DBNAME + "?parseTime=true"
+	db, err := gorm.Open(DBMS, dataSource)
 	if err != nil {
 		panic(err.Error())
 	}
 	return db
 }
-
-
